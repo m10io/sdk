13d2e35a1fa1c539d2af43747c572248ceda46ba
Daniel <x46085@gmail.com>
2022-03-18 17:25:19 -0700

    Clean Up

--- node/lib/utils/grpc/server-stream-request.js
+++
@@ -1,9 +0,0 @@
-const makeServerStreamRequest = function(client, originalFunction, options = {}) {
-  return function(content = {}, { timeout } = {}) {
-    const requestTimeout = options.timeout ?? timeout
-    const deadline = requestTimeout != null ? Date.now() + requestTimeout : null
-    return originalFunction.call(client, content, options.metadata, { deadline: deadline })
-  }
-}
-
-module.exports = makeServerStreamRequest

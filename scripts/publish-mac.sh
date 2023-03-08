d082aeaa07191b01eb43711eb9fc3dcd91f746a5
Andrei Khvalko <andrei.khvalko@m10.io>
2023-03-08 23:06:38 +0100

    Add basic actions for multiple releases

--- scripts/publish-mac.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-pushd rust
-pushd protos
-ln -s ../../protobuf/ protobuf
-sed -i '.bak' 's/..\/..\/protobuf/.\/protobuf/g' build.rs
-popd
-cargo workspaces publish --from-git --allow-dirty
-popd
